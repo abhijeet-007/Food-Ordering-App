7137c349d61b13126dd248d002a3dc2e6d69406c
unknown <abhijeetmodak642@gmail.com>
2026-05-18 22:45:00 +0530

    Fixed the fmt issue for all outputs.tf

--- terraform/envs/prod/outputs.tf
+++ terraform/envs/prod/outputs.tf
@@ -1,4 +1,4 @@
-output "alb_dns_name"        { value = module.compute.alb_dns_name }
-output "ecr_repository_url"  { value = module.compute.ecr_repository_url }
-output "ecs_cluster_name"    { value = module.compute.ecs_cluster_name }
-output "ecs_service_name"    { value = module.compute.ecs_service_name }
+output "alb_dns_name" { value = module.compute.alb_dns_name }
+output "ecr_repository_url" { value = module.compute.ecr_repository_url }
+output "ecs_cluster_name" { value = module.compute.ecs_cluster_name }
+output "ecs_service_name" { value = module.compute.ecs_service_name }
